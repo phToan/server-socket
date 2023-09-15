149f434474381648f7f77f61df3c464e3fa3d449
phToan <phamhaitoan10@gmail.com>
2023-09-15 18:21:55 +0700

    new dotenv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const app = express()
 const server = require("http").Server(app)
 const io = require("socket.io")(server)
-server.listen(3000)
+server.listen(process.env.PORT)
 let orders = [];
 const axios = require('axios')
 // const orders = new Map()
